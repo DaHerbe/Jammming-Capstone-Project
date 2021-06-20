f9cd6e532b0e816bceb84e62ece7f87238a5fa9d
DaHerbe <daherbe@gmail.com>
2021-06-20 16:45:59 -0400

    Added functionality for modifying the playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,32 @@ class App extends React.Component {
                   playlistTracks: [{name: "shut up", artist: "simple plan", album: "no idea", id: 12},
                                   {name: "shut up", artist: "blink 182", album: "something", id: 43},
                                   {name: "shut up", artist: "other band", album: "last thing", id: 137}]};
+    
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    //Checks to see if playlistTracks contains the id on the selected track
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    //Adds the new track to the list and sets the state
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    //Removes the track with the matching id from the list
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    //Sets the state with the new list without the specified track
+    this.setState({playlistTracks: tracks});
   }
 
   render() {
@@ -24,8 +50,10 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <Playlist playlistName={this.state.playlistName} 
+                      playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
