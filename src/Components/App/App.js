ba9003b990bfa6a3bceac79b8fd3a171d09589a6
DaHerbe <daherbe@gmail.com>
2021-06-20 16:10:41 -0400

    Pass down search results and render track list

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
 
-function App() {
-  return (
-    <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <SearchBar />
-    <div className="App-playlist">
-      <SearchResults />
-      <Playlist />
-    </div>
-  </div>
-</div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchResults: [{name: "shut up", artist: "simple plan", album: "no idea", id: 12},
+                                  {name: "shut up", artist: "blink 182", album: "something", id: 43},
+                                  {name: "shut up", artist: "other band", album: "last thing", id: 137}]};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
